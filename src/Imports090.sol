4979df97184796b081aa27456e8e82e32510af0b
j00sko <koprivec.filip@gmail.com>
2024-11-15 11:09:52 +0700

    Bump version

--- src/Imports090.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-// We import the contract so truffle compiles it, and we have the ABI
-// available when working from truffle console.
-import {FlareContractRegistryMock} from "lib/flare-foundry-periphery-package/src/coston2/mockContracts/MockFlareContractRegistry.sol";
--- src/Mock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.6;
-
-contract Mock {
-    fallback() external payable {
-        revert("Mock should have a mocked method");
-    }
-}
