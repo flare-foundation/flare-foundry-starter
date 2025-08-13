76e74a5a788cc962ce843b0778d9633ff7465045
Kristaps Grinbergs <kristaps@flare.network>
2025-08-13 18:28:12 +0300

    chore: update flare-periphery dependency to version 0.1.36 and refactor FAssetsSettings contract to remove asset manager address parameter

--- script/fassets/FAssetsSettings.s.sol
+++ script/fassets/FAssetsSettings.s.sol
@@ -9,15 +9,12 @@ import { FAssetsSettings } from "../../src/fassets/FAssetsSettings.sol";
 // forge script script/fassets/FAssetsSettings.s.sol:Deploy --private-key $PRIVATE_KEY --rpc-url $COSTON2_RPC_URL
 
 contract Deploy is Script {
-    // Address of the AssetManager contract on Flare Testnet Coston2
-    address constant ASSET_MANAGER = address(0xDeD50DA9C3492Bee44560a4B35cFe0e778F41eC5);
-
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the FAssetsSettings contract
-        FAssetsSettings fAssetsSettings = new FAssetsSettings(ASSET_MANAGER);
+        FAssetsSettings fAssetsSettings = new FAssetsSettings();
         console.log("FAssetsSettings deployed at:", address(fAssetsSettings));
 
         // Get lot size and decimals
